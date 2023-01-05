f3669d1ee37e9aa06bdbdef29206db90e409a235
Tu Nguyen Anh <tuna9x.it@gmail.com>
2023-01-05 21:54:09 +0700

    Update config compatible with turbopack

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { appDir: true },
-  images: {
-    domains: ["via.placeholder.com", "lh3.googleusercontent.com"],
-  },
+  experimental: { appDir: true }
 };
 
 module.exports = nextConfig;
