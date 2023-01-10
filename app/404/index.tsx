ddb9f36f5e960d7f2153e2008cd0c7bf8e03f7f7
tuna <tuna9x.it@gmail.com>
2023-01-10 16:53:21 +0700

    Update file

---
+++ app/404/index.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react';
+
+function index() {
+    return (
+        <React.Fragment>
+            <div className="inner">
+                <button className="btn">Hello button</button>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default index;
